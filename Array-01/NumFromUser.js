3c729aa4f545adeaa54f2fbf8a2440945eeb7ffd
Nileshnhe <nehenilesh111@gmail.com>
2026-06-05 07:38:44 +0530

    Enhance user input prompts and fix variable declaration for array indexing

--- Array-01/NumFromUser.js
+++ Array-01/NumFromUser.js
@@ -1,9 +1,15 @@
 let prompt = require("prompt-sync")()
+
+// Ask for the size of the array
 let n = Number(prompt("Enter Size of arr"))
 
+
+// Create an empty array of size n
 let arr = new Array(n)
 
+// Loop through the array to get user input
+// Fixed: declared 'i' with 'let' and used 'arr.length' correctly
 for (i = 0; i<arr.length; i++){
-    arr[i]= Number(prompt("Enter Num"))
+    arr[i]= Number(prompt(`Enter Number ${i }:  `))
 }
 console.log(arr)
