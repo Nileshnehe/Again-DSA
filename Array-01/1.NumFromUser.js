5fd984f1ea57a3680915a217b7596cf309358b5b
Nileshnhe <nehenilesh111@gmail.com>
2026-06-10 11:25:36 +0530

    Refactor array reversal logic and remove unused code

--- Array-01/1.NumFromUser.js
+++ Array-01/1.NumFromUser.js
@@ -13,3 +13,9 @@ for (i = 0; i<arr.length; i++){
     arr[i]= Number(prompt(`Enter Number ${i }:  `))
 }
 console.log(arr)
+//output:
+// Enter Size of arr3
+// Enter Number 0:  1
+// Enter Number 1:  2
+// Enter Number 2:  3
+// [ 1, 2, 3 ]
--- Array-01/5.ReverseArr.js
+++ Array-01/5.ReverseArr.js
@@ -1,12 +1,23 @@
 let arr = [22, 34, 54, 44];
 
-let temp = new Array(arr.length);
+// let temp = new Array(arr.length);
 
-let i = arr.length-1;
-for(let j = 0; j<temp.length; j++){
-    temp[j]=arr[i];
-    i--;
-}
+// let i = arr.length-1;
+// for(let j = 0; j<temp.length; j++){
+//     temp[j]=arr[i];
+//     i--;
+// }
 
-console.log(`Reverse Array: [${temp.join(', ')} ]`);
+// console.log(`Reverse Array: [${temp.join(', ')} ]`);
 // output: [ 44, 54, 34, 22 ]
+
+// optimize 
+let i = 0; let j = arr.length-1;
+while(i<j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+}
+console.log(arr);
